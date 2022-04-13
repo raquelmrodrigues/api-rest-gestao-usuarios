4c268361812c9dcb4e366db47d5026c0efa3cb9b
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-04-13 18:58:28 -0300

    validação dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,8 +2,25 @@ class UserController {
 
     async index(req, res){}
 
-    async create(req, res) {
+    async create(req, res){
         console.log(req.body);
+        var {email, name, password} = req.body;
+
+        if(email == undefined){
+            res.status(400);
+            res.json({err: "e-mail inválido"})
+        }
+        if(password == undefined){
+            res.status(400);
+            res.json({err: "senha inválida"})
+        }
+        if(name == undefined){
+            res.status(400);
+            res.json({err: "nome inválido"})
+        }
+
+        res.status(200);
+
         res.send("pegando o corpo da requisição");
     }
 }
